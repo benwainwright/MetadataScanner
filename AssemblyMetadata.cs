5e20419bfc8801ae81b007c7e1655a2c5fc56cba
Ben Wainwright <bwainwright28@gmail.com>
2018-02-16 23:24:19 +0000

    OK this is getting complicated. Looks like I'm going to have to write a custom wrapper around the metadata api

---
+++ AssemblyMetadata.cs
@@ -0,0 +1,81 @@
+﻿namespace CleanIoc.Metadata
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection.Metadata;
+    using System.Reflection.PortableExecutable;
+    using System.Runtime.InteropServices;
+
+    public class AssemblyMetadata : IDisposable
+    {
+        private MetadataReader reader;
+
+        private bool disposed = false;
+
+        private byte[] buffer;
+
+        private GCHandle pinnedHandle;
+
+        private List<AssemblyReference> assemblyReferences;
+
+        public AssemblyMetadata(string path)
+        {
+            FilePath = path;
+        }
+
+        public IEnumerable<AssemblyReference> AssemblyReferences
+        {
+            get
+            {
+                if (assemblyReferences == null) {
+                    assemblyReferences = AssemblyReference.LoadReferences(Reader);
+                }
+
+                return assemblyReferences;
+            }
+        }
+
+        public string FilePath { get; }
+
+        private MetadataReader Reader { get
+            {
+                if (reader == null) {
+                    reader = LoadMetadataReader(FilePath);
+                }
+
+                return reader;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed) {
+                if (disposing && reader != null) {
+                    pinnedHandle.Free();
+                }
+
+                disposed = true;
+            }
+        }
+
+        private unsafe MetadataReader LoadMetadataReader(
+            string filename,
+            MetadataReaderOptions options = MetadataReaderOptions.Default,
+            MetadataStringDecoder decoder = null)
+        {
+            buffer = File.ReadAllBytes(filename);
+            pinnedHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var headers = new PEHeaders(new MemoryStream(buffer));
+            var startOffset = headers.MetadataStartOffset;
+            var metaDataStart = (byte*)pinnedHandle.AddrOfPinnedObject() + startOffset;
+            return new MetadataReader(metaDataStart, headers.MetadataSize, options, decoder);
+        }
+    }
+}
---
+++ AssemblyReference.cs
@@ -0,0 +1,40 @@
+﻿namespace CleanIoc.Metadata
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Metadata;
+
+    public class AssemblyReference
+    {
+        public AssemblyReference(MetadataReader reader, AssemblyReferenceHandle handle)
+        {
+            var reference = reader.GetAssemblyReference(handle);
+            Name = reader.GetString(reference.Name);
+            Culture = reader.GetString(reference.Culture);
+            PublicKeyOrToken = reader.GetBlobBytes(reference.PublicKeyOrToken).ToList();
+            Version = reference.Version;
+            Flags = reference.Flags;
+        }
+
+        public string Culture { get; }
+
+        public string Name { get; }
+
+        public List<byte> PublicKeyOrToken { get; }
+
+        public Version Version { get; }
+
+        public AssemblyFlags Flags { get; }
+
+        public static List<AssemblyReference> LoadReferences(MetadataReader reader)
+        {
+            var query = from reference
+                        in reader.AssemblyReferences
+                        select new AssemblyReference(reader, reference);
+
+            return query.ToList();
+        }
+    }
+}
