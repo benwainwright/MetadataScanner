522278f93bd16e3529a0750d87570c6838aca192
Ben Wainwright <bwainwright28@gmail.com>
2018-02-19 09:28:15 +0000

    Added new test

---
+++ MetadataScanner.Test/TestScannedAssembly.cs
@@ -0,0 +1,19 @@
+﻿namespace MetadataScanner.Test
+{
+    using MetadataScanner.Enums;
+    using MetadataScanner.Lib;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestScannedAssembly
+    {
+        [Test]
+        public void TestScannedAssemblyWithTokenOnlyProducesAnUnresolvedAssembly()
+        {
+            var assembly = new ScannedAssembly(token: 1);
+
+            Assert.That(assembly.Token, Is.EqualTo(1));
+            Assert.That(assembly.ResolutionStatus, Is.EqualTo(ResolutionStatus.UnResolved));
+        }
+    }
+}
